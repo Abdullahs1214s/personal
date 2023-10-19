ab7faab2c4466df96b44511583275d35172ee9c6
Abdullahs1214s <148407508+Abdullahs1214s@users.noreply.github.com>
2023-10-19 03:02:39 +0000

    "we04"

---
+++ Python/preprocess/preprocess.py
@@ -0,0 +1,54 @@
+"""
+--------------------------------------------
+Name: xyz
+SID: xyz
+CCID: xyz
+AnonID: xyz
+CMPUT 274, Fall xyz
+
+Assessment: xyz
+--------------------------------------------
+
+TEMPLATE: ADD YOUR INFORMATION TO ABOVE
+
+You must determine how to structure your solution.
+Create your functions here and call them from under
+if __name__ == "__main__"!
+"""
+
+# Global List
+Stop_Words = [
+    "i", "me", "my", "myself", "we", "our",
+    "ours", "ourselves", "you", "your",
+    "yours", "yourself", "yourselves", "he",
+    "him", "his", "himself", "she", "her",
+    "hers", "herself", "it", "its", "itself",
+    "they", "them", "their", "theirs",
+    "themselves", "what", "which", "who",
+    "whom", "this", "that", "these", "those",
+    "am", "is", "are", "was", "were", "be",
+    "been", "being", "have", "has", "had",
+    "having", "do", "does", "did", "doing",
+    "a", "an", "the", "and", "but", "if",
+    "or", "because", "as", "until", "while",
+    "of", "at", "by", "for", "with",
+    "about", "against", "between", "into",
+    "through", "during", "before", "after",
+    "above", "below", "to", "from", "up",
+    "down", "in", "out", "on", "off", "over",
+    "under", "again", "further", "then",
+    "once", "here", "there", "when", "where",
+    "why", "how", "all", "any", "both",
+    "each", "few", "more", "most", "other",
+    "some", "such", "no", "nor", "not",
+    "only", "own", "same", "so", "than",
+    "too", "very", "s", "t", "can", "will",
+    "just", "don", "should", "now"
+    ]
+
+if __name__ == "__main__":
+    # Any code indented under this line will only be run
+    # when the program is called directly from the terminal
+    # using "python3 preprocess.py". This is directly relevant
+    # to this exercise, so you should call your code from here.
+    pass
